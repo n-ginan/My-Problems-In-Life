ab4ef7cdc341f3b1a3e521084f10fcdb0aa15e69
papsykle <toramgarcia@gmail.com>
2024-06-07 19:15:03 +0800

    hoyaaaa

---
+++ Codewars/kotlin/GrasshopperGameMove.kt
@@ -0,0 +1,15 @@
+fun main() {
+    print(move(3, 6))
+}
+
+fun move(pos: Int, roll: Int): Int = pos + (roll * 2)
+
+/*
+Terminal game move function
+
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.    
+
+move(3, 6) should equal 15
+*/
