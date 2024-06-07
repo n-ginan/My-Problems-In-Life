b2d1c28a05b1174f96fd4a06b74991492dfdfdfa
papsykle <toramgarcia@gmail.com>
2024-06-07 16:42:54 +0800

    donezo

---
+++ Codewars/kotlin/AsciiValue.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print(getAscii('B'))
+} 
+
+fun getAscii(c: Char): Int = c.code
+
+/*
+    Get ASCII value of a character.
+
+    For the ASCII table you can refer to http://www.asciitable.com/
+*/
