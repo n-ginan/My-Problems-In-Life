2413d7ba1a2551d5f85ec771ecb63c1389e49b94
papsykle <toramgarcia@gmail.com>
2024-06-10 19:51:05 +0800

    did at friends house

---
+++ Leetcode/kotlin/Permutation3146.kt
@@ -0,0 +1,40 @@
+fun findPermutationDifference(s: String, t: String): Int {
+    var sum = 0
+    for (i in 0 until s.length) {
+        sum +=  Math.abs(s.indexOf(s[i]) - t.indexOf(s[i]))
+    }
+    return sum
+}
+
+/*
+You are given two strings s and t such that every character occurs at most once in s and t is a permutation of s.
+
+The permutation difference between s and t is defined as the sum of the absolute difference between the index of the occurrence of each character in s and the index of the occurrence of the same character in t.
+
+Return the permutation difference between s and t.
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "bac"
+
+Output: 2
+
+Explanation:
+
+For s = "abc" and t = "bac", the permutation difference of s and t is equal to the sum of:
+
+The absolute difference between the index of the occurrence of "a" in s and the index of the occurrence of "a" in t.
+The absolute difference between the index of the occurrence of "b" in s and the index of the occurrence of "b" in t.
+The absolute difference between the index of the occurrence of "c" in s and the index of the occurrence of "c" in t.
+That is, the permutation difference between s and t is equal to |0 - 1| + |2 - 2| + |1 - 0| = 2.
+
+Example 2:
+
+Input: s = "abcde", t = "edbac"
+
+Output: 12
+
+Explanation: The permutation difference between s and t is equal to |0 - 3| + |1 - 2| + |2 - 4| + |3 - 1| + |4 - 0| = 12.
+*/
